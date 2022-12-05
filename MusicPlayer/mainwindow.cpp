d186d10559c062c4a5aba599434ea074d4588157
敖星 <1485219696@qq.com>
2022-12-06 00:00:52 +0800

    woshi nide i

--- MusicPlayer/mainwindow.cpp
+++ MusicPlayer/mainwindow.cpp
@@ -1,11 +1,12 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-
+#include <QDebug>
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    qDebug()<<"hapizhaofeng";
 }
 
 MainWindow::~MainWindow()
